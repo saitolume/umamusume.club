83420930be3027ba4ebf6897c8d371f02b1ca5bf
Tadao Iseki <saitoeku3@gmail.com>
2021-03-01 22:54:23 +0900

    feat: ヘッダーを追加

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import { memo, VFC } from 'react'
+
+const Header: VFC = () => (
+  <header className="text-uma-text3 bg-uma-surface2 h-14 flex items-center justify-center font-bold border-uma-border2 border-b-2 rounded-br-lg rounded-bl-lg">
+    <Link href="/">
+      <a>
+        <h1>ウマ娘 プリティーダービー フレンド掲示板</h1>
+      </a>
+    </Link>
+  </header>
+)
+
+export default memo(Header)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
 import React from 'react'
+import Header from '~/components/Header'
+import 'tailwindcss/tailwind.css'
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => (
   <>
-    <header></header>
+    <Header />
     <main>
       <Component {...pageProps} />
     </main>
